56bf9af7be868b5906dc0eb85a629da3a63339fa
ayite1207 <ayite.folly@gmail.com>
2021-09-30 16:06:54 +0200

    display message when there are no results in searchView

---
+++ MangaUniver/MangaUniver/View/Search/EmptyCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  EmptyCollectionViewCell.swift
+//  MangaUniver
+//
+//  Created by ayite on 29/09/2021.
+//
+
+import UIKit
+
+class EmptyCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
